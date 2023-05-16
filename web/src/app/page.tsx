2460fa1a1a5a9f96ded8c29ff5b7777cfb387dda
savio <savioanjos2020@gmail.com>
2023-05-16 11:40:56 -0300

    Finalizando setup web

--- web/src/app/page.tsx
+++ web/src/app/page.tsx
@@ -1,3 +1,5 @@
 export default function Home() {
-  return <div className="m-10 w-1 text-2xl font-bold uppercase">Oi</div>
+  return (
+    <div className="m-10 w-1 bg-red-500 text-2xl font-bold uppercase">Oi</div>
+  )
 }
