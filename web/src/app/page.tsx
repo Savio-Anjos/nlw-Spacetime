6b4274dccc09ffe891af6cb07a14a4359aa0e637
savio <savioanjos2020@gmail.com>
2023-05-17 11:08:52 -0300

    Configurando fontes

--- web/src/app/page.tsx
+++ web/src/app/page.tsx
@@ -1,7 +1,8 @@
 export default function Home() {
   return (
-    <div className="h-screen bg-zinc-950 p-6 text-zinc-50">
+    <div className="h-screen items-center justify-center bg-zinc-950 p-6 text-zinc-50">
       <h1 className="text-4xl font-bold">Sua cápsula do tempo</h1>
+      <h1 className="font-alt text-4xl font-bold">Sua cápsula do tempo</h1>
     </div>
   )
 }
