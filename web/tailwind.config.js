9553d4e49bd4cfb46d5df25df9fb3ff223cb33fd
savio <savioanjos2020@gmail.com>
2023-05-17 12:54:57 -0300

    Criando Stripes

--- web/tailwind.config.js
+++ web/tailwind.config.js
@@ -11,6 +11,7 @@ module.exports = {
         sans: 'var(--font-roboto)',
         alt: 'var(--font-bai-jamjuree)',
       },
+
       colors: {
         gray: {
           50: '#eaeaea',
@@ -48,6 +49,19 @@ module.exports = {
           800: '#027435',
           900: '#025929',
         },
+      },
+
+      backgroundImage: {
+        stripes:
+          'linear-gradient(to bottom, rgba(255, 255, 255, 0.1), rgba(255, 255, 255, 0.1) 12.5%, transparent 12.5%, transparent)',
+      },
+
+      fontSize: {
+        '5xl': '2.5rem',
+      },
+
+      backgroundSize: {
+        stripes: '100% 8px',
       },
 
       blur: {
