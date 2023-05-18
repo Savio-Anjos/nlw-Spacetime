abf2d349e195de5154a2bce435f3d36652413e2e
savio <savioanjos2020@gmail.com>
2023-05-18 15:39:51 -0300

    Criando area Hero

--- web/src/app/page.tsx
+++ web/src/app/page.tsx
@@ -1,5 +1,9 @@
 import { User } from 'lucide-react'
 
+import Image from 'next/image'
+
+import nlwLogo from '../assets/nlw-spacetime-logo.svg'
+
 export default function Home() {
   return (
     <main className="grid min-h-screen grid-cols-2">
@@ -25,6 +29,27 @@ export default function Home() {
             memórias!
           </p>
         </a>
+
+        {/* Hero */}
+        <div className="space-y-5">
+          <Image src={nlwLogo} alt="NLW Spacetime" />
+
+          <div className="max-w-[420px] space-y-1">
+            <h1 className="text-5xl font-bold leading-tight text-gray-50">
+              Sua cápsula do tempo
+            </h1>
+            <p className="text-lg leading-relaxed">
+              Colecione momentos marcantes da sua jornada e compartilhe (se
+              quiser) com o mundo!
+            </p>
+          </div>
+          <a
+            className="inline-block rounded-full bg-green-500 px-5 py-3 font-alt text-sm uppercase leading-none text-black hover:bg-green-600"
+            href=""
+          >
+            CADASTRAR LEMBRANÇA
+          </a>
+        </div>
       </div>
 
       {/* Right */}
