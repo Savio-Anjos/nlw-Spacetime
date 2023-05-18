bf6e18bcf27a91fe07b7f95c7b24280cbc333611
savio <savioanjos2020@gmail.com>
2023-05-18 16:14:07 -0300

    Criando Copyright

--- web/src/app/page.tsx
+++ web/src/app/page.tsx
@@ -50,6 +50,20 @@ export default function Home() {
             CADASTRAR LEMBRANÇA
           </a>
         </div>
+
+        {/* Copyright */}
+
+        <div className="text-sm leading-relaxed text-gray-200">
+          Feito com 💜 no NLW da{' '}
+          <a
+            target="_blank"
+            className="underline hover:text-gray-100"
+            href="https://rocketseat.com.br"
+            rel="noreferrer"
+          >
+            Rocketseat
+          </a>{' '}
+        </div>
       </div>
 
       {/* Right */}
