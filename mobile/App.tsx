dc23889dba01d6bdc16274c663f89233777a7ad5
savio <savioanjos2020@gmail.com>
2023-05-18 17:04:18 -0300

    Configurando fontes no mobile

--- mobile/App.tsx
+++ mobile/App.tsx
@@ -1,10 +1,29 @@
 import { StatusBar } from 'expo-status-bar'
 import { Text, View } from 'react-native'
 
+import { BaiJamjuree_700Bold } from '@expo-google-fonts/bai-jamjuree'
+
+import {
+  useFonts,
+  Roboto_400Regular,
+  Roboto_700Bold,
+} from '@expo-google-fonts/roboto'
+
 export default function App() {
+  const [hasLoadedFonts] = useFonts({
+    Roboto_400Regular,
+    Roboto_700Bold,
+    BaiJamjuree_700Bold,
+  })
+
+  if (!hasLoadedFonts) {
+    return null
+  }
+
   return (
-    <View className="flex-1 items-center justify-center bg-gray-950">
-      <Text className="text-5xl font-bold text-zinc-50">Rocketseat</Text>
+    <View className="flex-1 items-center justify-center bg-gray-900">
+      <Text className="font-body text-5xl text-gray-50">Rocketseat</Text>
+      <Text className="font-alt text-5xl text-gray-50">Rocketseat</Text>
 
       <StatusBar style="light" translucent />
     </View>
--- mobile/tailwind.config.js
+++ mobile/tailwind.config.js
@@ -3,6 +3,12 @@ module.exports = {
   content: ['./App.tsx', './app/**/*.tsx'],
   theme: {
     extend: {
+      fontFamily: {
+        title: 'Roboto_700Bold',
+        body: 'Roboto_400Regular',
+        alt: 'BaiJamjuree_700Bold',
+      },
+
       colors: {
         gray: {
           50: '#eaeaea',
