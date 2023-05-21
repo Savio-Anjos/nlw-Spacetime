b8edf6979a100b450b96b022f6e509898b24d7fc
savio <savioanjos2020@gmail.com>
2023-05-21 12:16:12 -0300

    Criando rota para deletar Memória

--- server/src/routes/memories.ts
+++ server/src/routes/memories.ts
@@ -35,9 +35,40 @@ export async function memoriesRoutes(app: FastifyInstance) {
     return memory
   })
 
-  app.post('/memories', async () => {})
+  app.post('/memories', async (request) => {
+    const bodySchema = z.object({
+      content: z.string(),
+      coverUrl: z.string(),
+      isPublic: z.coerce.boolean().default(false),
+    })
+
+    const { content, coverUrl, isPublic } = bodySchema.parse(request.body)
+
+    const memory = await prisma.memory.create({
+      data: {
+        content,
+        coverUrl,
+        isPublic,
+        userId: 'b16ee033-77d1-4f74-a665-ab7afa801bbf',
+      },
+    })
+
+    return memory
+  })
 
   app.put('/memories/:id', async () => {})
 
-  app.delete('/memories', async () => {})
+  app.delete('/memories', async (request) => {
+    const paramsSchema = z.object({
+      id: z.string().uuid(),
+    })
+
+    const { id } = paramsSchema.parse(request.params)
+
+    await prisma.memory.delete({
+      where: {
+        id,
+      },
+    })
+  })
 }
