be04b7c5c22598d0191d24615123443797203371
savio <savioanjos2020@gmail.com>
2023-05-21 14:20:01 -0300

    Criando rota para Atualizar Memória

--- server/src/routes/memories.ts
+++ server/src/routes/memories.ts
@@ -56,7 +56,34 @@ export async function memoriesRoutes(app: FastifyInstance) {
     return memory
   })
 
-  app.put('/memories/:id', async () => {})
+  app.put('/memories/:id', async (request) => {
+    const paramsSchema = z.object({
+      id: z.string().uuid(),
+    })
+
+    const { id } = paramsSchema.parse(request.params)
+
+    const bodySchema = z.object({
+      content: z.string(),
+      coverUrl: z.string(),
+      isPublic: z.coerce.boolean().default(false),
+    })
+
+    const { content, coverUrl, isPublic } = bodySchema.parse(request.body)
+
+    const memory = await prisma.memory.update({
+      where: {
+        id,
+      },
+      data: {
+        content,
+        coverUrl,
+        isPublic,
+      },
+    })
+
+    return memory
+  })
 
   app.delete('/memories', async (request) => {
     const paramsSchema = z.object({
