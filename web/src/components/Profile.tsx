8bd3c55601d91066ce91a29de414132ad30d15a9
savio <savioanjos2020@gmail.com>
2023-05-22 15:52:43 -0300

    Finalizando perfil de usuário

---
+++ web/src/components/Profile.tsx
@@ -0,0 +1,19 @@
+/* eslint-disable prettier/prettier */
+import { getUser } from "@/lib/auth";
+import Image from "next/image";
+
+export function Profile() {
+    const { name, avatarUrl } = getUser()
+    return (
+        <div
+            className="flex items-center gap-3 text-left "
+        >
+            <Image src={avatarUrl} width={40} height={40} alt="" className="w-10 h-10 rounded-full" />
+
+            <p className="max-w-[140px] text-sm leading-snug">
+                {name}
+                <a href="" className="block text-red-400 hover:text-red-300">Quero sair</a>
+            </p>
+        </div>
+    )
+}
