d68f542d9b03287f6ae0faa6074de1013ad5a6c0
savio <savioanjos2020@gmail.com>
2023-05-22 13:50:27 -0300

    Recuperando código recebido do Github

---
+++ web/src/app/api/auth/callback/route.ts
@@ -0,0 +1,6 @@
+import { NextRequest } from 'next/server'
+
+export async function GET(request: NextRequest) {
+  const { searchParams } = new URL(request.url)
+  const code = searchParams.get('code')
+}
