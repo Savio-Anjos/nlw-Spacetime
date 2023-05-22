e79d9d48cfc4ca18f3f0902af98498f14a5a02c3
savio <savioanjos2020@gmail.com>
2023-05-21 21:00:18 -0300

    Componentizando o Hero

--- web/src/app/page.tsx
+++ web/src/app/page.tsx
@@ -15,19 +15,6 @@ export default function Home() {
         <div className="absolute bottom-0 right-2 top-0 w-2 bg-stripes" />
 
         {/* Sign In */}
-        <a
-          href={`https://github.com/login/oauth/authorize?client_id=${process.env.NEXT_PUBLIC_GITHUB_CLIENT_ID}`}
-          className="flex items-center gap-3 text-left transition-colors hover:text-gray-50"
-        >
-          <div className="flex h-10 w-10 items-center justify-center rounded-full bg-gray-400">
-            <User className="h-5 w-5 text-gray-500" />
-          </div>
-
-          <p className="max-w-[140px] text-sm leading-snug">
-            <span className="underline">Crie sua conta</span> e salve suas
-            memórias!
-          </p>
-        </a>
 
         {/* Hero */}
         <Hero />
