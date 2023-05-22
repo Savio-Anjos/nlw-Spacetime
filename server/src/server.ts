006caae8a256abfb511eba90a45cf5c4558a0927
savio <savioanjos2020@gmail.com>
2023-05-22 10:40:17 -0300

    Criando token JWT

--- server/src/server.ts
+++ server/src/server.ts
@@ -2,6 +2,7 @@ import 'dotenv/config'
 
 import fastify from 'fastify'
 import cors from '@fastify/cors'
+import jwt from '@fastify/jwt'
 
 import { memoriesRoutes } from './routes/memories'
 import { authRoutes } from './routes/auth'
@@ -11,6 +12,11 @@ const app = fastify()
 app.register(cors, {
   origin: true,
 })
+
+app.register(jwt, {
+  secret: 'spacetime',
+})
+
 app.register(authRoutes)
 app.register(memoriesRoutes)
 
