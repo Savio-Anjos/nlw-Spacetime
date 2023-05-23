a9128bb0b5ecc9fedc9a3743d23bb74d95471830
savio <savioanjos2020@gmail.com>
2023-05-22 22:18:35 -0300

    Utilizando expo router

---
+++ mobile/app/memories.tsx
@@ -0,0 +1,10 @@
+/* eslint-disable prettier/prettier */
+import { Text, View } from 'react-native'
+
+export default function Memories() {
+    return (
+        <View className="flex-1 items-center justify-center">
+            <Text>Memories</Text>
+        </View>
+    )
+}
