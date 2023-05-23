974526a0067e09b8fe6fc00a3843e13f8f6c120d
savio <savioanjos2020@gmail.com>
2023-05-23 09:44:59 -0300

    Criando rota de logout

--- web/src/components/Profile.tsx
+++ web/src/components/Profile.tsx
@@ -12,7 +12,7 @@ export function Profile() {
 
             <p className="max-w-[140px] text-sm leading-snug">
                 {name}
-                <a href="" className="block text-red-400 hover:text-red-300">Quero sair</a>
+                <a href="/api/auth/logout" className="block text-red-400 hover:text-red-300">Quero sair</a>
             </p>
         </div>
     )
