9ab974be49c2ffc38edc31be2a6bdfc7ba1c77a5
savio <savioanjos2020@gmail.com>
2023-05-24 19:22:55 -0300

    Finalizando Component do formuário

--- web/src/components/NewMemoryForm.tsx
+++ web/src/components/NewMemoryForm.tsx
@@ -3,10 +3,19 @@
 
 import { Camera } from "lucide-react";
 import MediaPicker from "./MediaPicker";
+import { FormEvent } from "react";
 
 export function NewMemoryForm() {
+    function handleCreateMemory(event: FormEvent<HTMLFormElement>) {
+        event.preventDefault()
+
+        const formaData = new FormData(event.currentTarget)
+
+        console.log(Array.from(formaData.entries()))
+    }
+
     return (
-        <form onSubmit={() => console.log('submit')} className="flex flex-1 flex-col gap-2">
+        <form onSubmit={handleCreateMemory} className="flex flex-1 flex-col gap-2" >
             <div className="flex items-center gap-4">
                 <label
                     htmlFor="media"
@@ -39,6 +48,12 @@ export function NewMemoryForm() {
                 className="w-full flex-1 resize-none rounded border-0 bg-transparent p-0 text-lg leading-relaxed text-gray-100 placeholder:text-gray-400 focus:ring-0"
                 placeholder="Fique livre para adicionar fotos, vídeos e relatos sobre essa experiência que você quer lembrar para sempre."
             />
-        </form>
+            <button
+                type="submit"
+                className="inline-block self-end rounded-full bg-green-500 px-5 py-3 font-alt text-sm uppercase leading-none text-black hover:bg-green-600"
+            >
+                Salvar
+            </button>
+        </form >
     )
 }
