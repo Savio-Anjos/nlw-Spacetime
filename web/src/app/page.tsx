081afc70836f70cddc4d9001913a3e758006c796
savio <savioanjos2020@gmail.com>
2023-05-25 15:25:50 -0300

    Listando memórias

--- web/src/app/page.tsx
+++ web/src/app/page.tsx
@@ -3,6 +3,9 @@ import { api } from '@/lib/api'
 import { cookies } from 'next/headers'
 import dayjs from 'dayjs'
 import ptBR from 'dayjs/locale/pt-br'
+import Image from 'next/image'
+import Link from 'next/link'
+import { ArrowRight } from 'lucide-react'
 
 dayjs.locale(ptBR)
 
@@ -19,7 +22,9 @@ export default async function Home() {
   if (!isAuthenticated) {
     return <EmptyMemories />
   }
+
   const token = cookies().get('token')?.value
+
   const response = await api.get('/memories', {
     headers: {
       Authorization: `Bearer ${token}`,
@@ -40,6 +45,23 @@ export default async function Home() {
             <time className="-ml-8 flex items-center gap-2 text-sm text-gray-100 before:h-px before:w-5 before:bg-gray-50">
               {dayjs(memory.createdAt).format('D[ de ]MMMM[, ]YYYY')}
             </time>
+            <Image
+              src={memory.coverUrl}
+              alt=""
+              width={592}
+              height={280}
+              className="aspect-video w-full rounded-lg object-cover"
+            />
+            <p className="text-lg leading-relaxed text-gray-100">
+              {memory.excerpt}
+            </p>
+            <Link
+              href={`/memories/${memory.id}`}
+              className="flex items-center gap-2 text-sm text-gray-200 hover:text-gray-100"
+            >
+              Ler mais
+              <ArrowRight className="h-4 w-4" />
+            </Link>
           </div>
         )
       })}
